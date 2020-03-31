c9e3f29f346809bbe361cf43930fe5cea229293b
Evan Smith <professionalemail@zoho.com>
2020-03-31 00:12:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   my_array = string.split
   my_array2 = []
     my_array.collect do |word|
-    may_array2 << word.reverse
+    my_array2 << word.reverse
     end
     reversed_sentence = my_array2.join(" ")
     return reversed_sentence
